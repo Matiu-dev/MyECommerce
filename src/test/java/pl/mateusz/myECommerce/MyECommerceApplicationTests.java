d67cc3ecb17eebd40e60adf64a817ee687e2832f
mwroblewski-at-596854579163 <S0dnO6NaXKpyiRDM0szh0novLXuFYiyt+ELGVwgREII=>
2020-11-03 22:04:11 +0100

    Initial commit

---
+++ src/test/java/pl/mateusz/myECommerce/MyECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mateusz.myECommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
