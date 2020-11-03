2284841aa1104120c8ff13dcf69cf857380c1826
mwroblewski-at-596854579163 <S0dnO6NaXKpyiRDM0szh0novLXuFYiyt+ELGVwgREII=>
2020-11-04 00:01:45 +0100

    simple hello world

--- src/main/java/pl/mateusz/myECommerce/MyECommerceApplication.java
+++ src/main/java/pl/mateusz/myECommerce/MyECommerceApplication.java
@@ -2,12 +2,27 @@ package pl.mateusz.myECommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class MyECommerceApplication {
+@RestController
+public class MyECommerceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyECommerceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyECommerceApplication.class, args);
+	}
+
+	@RequestMapping(value = "/")
+	public String hello(){
+		return "Hello world";
 	}
 
 }
